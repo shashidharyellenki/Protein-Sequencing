830476d1f33cb849fa36685370ed1e7ed7175351
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2021-11-08 09:52:04 +0530

    Convert DNA to RNA

--- hw6_protein.py
+++ hw6_protein.py
@@ -31,8 +31,23 @@ Parameters: str ; int
 Returns: list of strs
 '''
 def dnaToRna(dna, startIndex):
+    res=dna[startIndex:]
+    list_=[]
+    string=""
+    ignore=["UAG", "UAA","UGA"]
+    for letter in range(len(res)):
+        if len(string)!=3:
+            string+=res[letter]
+        if len(string)==3:
+            x= string.replace("T","U")
+            if x in ignore:
+                list_.append(x)
+                return list_
+            else:
+                list_.append(x)
+                string=""
 
-    return
+    return list_
 
 
 '''
